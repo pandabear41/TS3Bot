da171f55a99d39eeee410a502fa8fa33e4c131d9
Xeronel <ripster1121@gmail.com>
2013-06-24 22:13:27 -0400

    Updated telnet.connect

--- TS3Bot.py
+++ TS3Bot.py
@@ -3,11 +3,20 @@ Created on Jun 24, 2013
 
 @author: TReische
 '''
+import time
+import telnet
+import sys
 
-if __name__ == '__main__':
-#    import sys
-    import telnet
-    
+def main_loop():
     tn = telnet.telnet()
     tn.connect()
-    
+        
+    while 1:
+        time.sleep(0.1)
+           
+
+if __name__ == '__main__':
+    try:
+        main_loop()
+    except KeyboardInterrupt:
+        sys.exit() 
