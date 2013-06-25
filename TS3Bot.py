d14e2f32ba7371fec5c652ecdf6615fd3e0adb75
Xeronel <ripster1121@gmail.com>
2013-06-25 00:42:25 -0400

    Moved connection to be global so it is persistent

--- TS3Bot.py
+++ TS3Bot.py
@@ -6,17 +6,16 @@ Created on Jun 24, 2013
 import time
 import telnet
 import sys
-
+    
 def main_loop():
     tn = telnet.telnet()
     tn.connect()
-        
+    
     while 1:
         time.sleep(0.1)
            
-
 if __name__ == '__main__':
     try:
         main_loop()
     except KeyboardInterrupt:
-        sys.exit() 
+        sys.exit()
--- telnet.py
+++ telnet.py
@@ -2,47 +2,49 @@ import telnetlib
 import ConfigParser
 import sys
 
+#Get config
+Config = ConfigParser.RawConfigParser(allow_no_value=False)
+Config.read('config.ini')
+
+#Read config properties
+User = Config.get('config', 'server_query_user')
+Pass = Config.get('config', 'server_query_pass')
+Host = Config.get('config', 'server_address')
+Port = Config.get('config', 'server_query_port')
+VID = Config.get('config', 'virtualserver_id')
+
+#Set command accepted message
+accepted = "\n\rerror id=0 msg=ok"
+
+tn = telnetlib.Telnet(Host, Port)   #connect to server
+    
 class telnet():
-    def connect(self):
-        #Get config
-        Config = ConfigParser.RawConfigParser(allow_no_value=False)
-        Config.read('config.ini')
-        
-        #Read config properties
-        User = Config.get('config', 'server_query_user')
-        Pass = Config.get('config', 'server_query_pass')
-        Host = Config.get('config', 'server_address')
-        Port = Config.get('config', 'server_query_port')
-        VID = Config.get('config', 'virtualserver_id')
-        
-        #Set command accepted message
-        accepted = "ok"
-        acceptmsg = "\n\rerror id=0 msg=ok"
-        
-        tn = telnetlib.Telnet(Host, Port)   #connect to server
+    def connect(self):    
+
         msg = tn.read_until(".", 2)         #wait until the welcome message is displayed
         sys.stdout.write(msg)               #print the welcome message
         
         tn.write("login " + User + " " + Pass + "\n")   #send login string
         msg = tn.read_until(accepted, 2)                #wait for login to be accepted or denied
-       
-        if msg == acceptmsg:                        #if login is accepted
+        
+        if msg == accepted:                         #if login is accepted
             sys.stdout.write(msg.replace("\s"," ")) #output message to console
             tn.write("use " + VID + "\n")           #select the virtual server
             msg = tn.read_until(accepted, 2)
             
             #if command is not accepted disconnect and return false
-            if msg != acceptmsg:
+            if msg != accepted:
                 sys.stdout.write("\n\rError selecting virtual server " + VID)
                 tn.write("logout\n")
                 tn.read_until(accepted, 2)
                 tn.write("quit\n")
                 return False
                 
-            sys.stdout.write('"' + msg.replace("\s"," ") + "'")
-            #tn.write("quit\n")
+            sys.stdout.write(msg.replace("\s"," "))
+            return True
         else:
             sys.stdout.write("\r\nError logging in. Please check your username and password.")
             sys.stdout.write(msg.replace("\s", " "))
             tn.write("quit\n")
             return False
+        
