5f323276c9e4ffc76aafd9e1bd837e5511ad4f81
Xeronel <ripster1121@gmail.com>
2013-06-24 20:59:01 -0400

    Updated stuff

--- program.py
+++ program.py
@@ -8,29 +8,6 @@ if __name__ == '__main__':
 #    import sys
     import telnet
     
-    user = "serveradmin"
-    password = "GgDqiM3G"
-    msg = None
-    
     tn = telnet.telnet()
-    tn.connect("halfcrap.com", "10011", 30)
-    
-    #tn = telnetlib.Telnet("halfcrap.com", "10011")      #Connect to the server
-#    msg = tn.read_until("a specific command.")          #Wait until the welcome message is displayed
-#    sys.stdout.write(msg)                               #Print the welcome message
-    
-#    tn.write("login " + user + " " + password + "\n")   #send login string
-#    msg = tn.read_until("\n\rerror id=0 msg=ok", 2)
-    
-#    if msg == "\n\rerror id=0 msg=ok":
-#        sys.stdout.write(msg.replace("\s"," "))
-#        tn.write("use 1\n")
-#        msg = tn.read_until("\n\rerror id=0 msg=ok")
-#        sys.stdout.write('"' + msg.replace("\s"," ") + "'")
-#        tn.write("quit\n")
-#    else:
-#        sys.stdout.write(msg.replace("\s", " "))
-#        tn.write("quit\n")
-#
-#    print tn.read_all().replace("\s", " ")
+    tn.connect()
     
--- telnet.py
+++ telnet.py
@@ -2,18 +2,45 @@ class telnet():
     def connect(self):
         import telnetlib
         import ConfigParser
-        #import io
+        import sys
         
+        #Get config
         Config = ConfigParser.RawConfigParser(allow_no_value=False)
         Config.read('config.ini')
         
-        User = Config.get('config', 'User')
-        Pass = Config.get('config', 'Pass')
-        Host = Config.get('config', 'Host')
-        Port = Config.get('config', 'Port')
-        Timeout = Config.get('config', 'Timeout')
+        #Read config properties
+        User = Config.get('config', 'server_query_user')
+        Pass = Config.get('config', 'server_query_pass')
+        Host = Config.get('config', 'server_address')
+        Port = Config.get('config', 'server_query_port')
+        VID = Config.get('config', 'virtualserver_id')
         
-        tn = telnetlib.Telnet(Host, Port, Timeout)
+        #Set command accepted message
+        accepted = "\n\rerror id=0 msg=ok"
         
+        tn = telnetlib.Telnet(Host, Port)               #connect to server
+        msg = tn.read_until("a specific command.", 2)   #wait until the welcome message is displayed
+        sys.stdout.write(msg)                           #print the welcome message
         
-        
+        tn.write("login " + User + " " + Pass + "\n")   #send login string
+        msg = tn.read_until(accepted, 2)                #wait for login to be accepted or denied
+       
+        if msg == accepted:                         #if login is accepted accepted
+            sys.stdout.write(msg.replace("\s"," ")) #output message to console
+            tn.write("use " + VID + "\n")           #select the virtual server
+            msg = tn.read_until(accepted, 2)        #wait for the command to be accepted
+            
+            if msg != accepted:     #if command is not accepted disconnect and return false
+                sys.stdout.write("Error selecting virtual server # " + VID)
+                tn.write("quit")
+                return False
+                
+            sys.stdout.write('"' + msg.replace("\s"," ") + "'")
+            tn.write("quit\n")
+        else:
+            sys.stdout.write("Error logging in. Please check your username and password.")
+            sys.stdout.write(msg.replace("\s", " "))
+            tn.write("quit\n")
+            return False
+    
+        print tn.read_all().replace("\s", " ")
