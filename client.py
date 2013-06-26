10bfe0adf8932d6f2f3b3cad30e6ec908ade6ec1
TReische <treische@wesco.com>
2013-06-26 16:40:30 -0400

    initial commit

---
+++ client.py
@@ -0,0 +1,9 @@
+class client():
+    #clid            #client id            (based on connection order)
+    #cid             #channel id           (current channel id)
+    #servergroups    #client_servergroups  (2,3,8)
+    #away            #client_away          (0|1)
+    
+    def idle_time(self):    #client_idle_time     (milliseconds)
+        return "hello world"
+    
---
+++ test.py
@@ -0,0 +1,5 @@
+import client
+
+if __name__ == '__main__':
+    cli = client.client()
+    print cli.idle_time()
