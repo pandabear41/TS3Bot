68306c4eeb5cb9f15eafaff19afddc7f5ff6b7b2
TReische <treische@wesco.com>
2013-06-28 10:30:38 -0400

    ignore SQClients & get update interval from cfg

--- TS3Bot.py
+++ TS3Bot.py
@@ -10,40 +10,43 @@ import ConfigParser
 import teamspeak
 
 class cfg():
-    sq_user = ''
-    sq_pass = ''
-    sq_port = ''
-    server_address = ''
-    vsid = ''
-    afk_channel = ''
+    sq_user = None
+    sq_pass = None
+    sq_port = None
+    server_address = None
+    vsid = None
+    idle_cid = None
+    update_interval = None
     
 def ReadConfig():
-    Config = ConfigParser.RawConfigParser(allow_no_value=True)
+    Config = ConfigParser.RawConfigParser(allow_no_value=False)
     Config.read('config.ini')
     
-    cfg.sq_user = Config.get('config', 'server_query_user')
-    cfg.sq_pass = Config.get('config', 'server_query_pass')
-    cfg.sq_port = Config.get('config', 'server_query_port')
-    cfg.server_address = Config.get('config', 'server_address')
-    cfg.vsid = Config.get('config', 'virtualserver_id')
-    cfg.idle_cid = Config.get('config', 'idle_channel_id')
-
+    conf = cfg()
+    conf.sq_user = Config.get('config', 'server_query_user')
+    conf.sq_pass = Config.get('config', 'server_query_pass')
+    conf.sq_port = Config.get('config', 'server_query_port')
+    conf.server_address = Config.get('config', 'server_address')
+    conf.vsid = Config.get('config', 'virtualserver_id')
+    conf.idle_cid = Config.get('config', 'idle_channel_id')
+    conf.update_interval = int(Config.get('config', 'update_interval'))
+    return conf
     
 def main_loop():
-    ReadConfig()
+    conf = ReadConfig()
     ts = teamspeak.TS3()
-    ts.connect(cfg.sq_user, cfg.sq_pass, cfg.server_address, cfg.sq_port, cfg.vsid)
+    ts.connect(conf.sq_user, conf.sq_pass, conf.server_address, conf.sq_port, conf.vsid)
     
     while 1:
-        seconds = int(datetime.datetime.now().strftime('%S')) % 30
+        seconds = int(datetime.datetime.now().strftime('%S')) % conf.update_interval
         
         if seconds == 0:
             clist = ts.clientlist()
             for c in clist:
                 client = ts.clientinfo(c)
-                if client.idletime >= 1 and client.cid != cfg.idle_cid:
-                    ts.move(c, cfg.idle_cid)
-            
+                if client.idletime >= 180 and client.cid != cfg.idle_cid and client.type != '1':
+                    ts.move(c, conf.idle_cid)
+
         time.sleep(0.1)
            
            
--- teamspeak.py
+++ teamspeak.py
@@ -7,49 +7,46 @@ class TS3Client():
     idletime = 0           #client idle time     (milliseconds)
     away = '0'             #client_away          (0|1)
     servergroups = []      #client_servergroups  (2,3,8)
+    type = '0'             #client_type          (0 = normal|1 = serverquery)
     
 class TS3():
     tn = telnetlib.Telnet()
     accepted = "\n\rerror id=0 msg=ok"
     
+    #connects to teamspeak and selects the virtual server
     def connect(self, User, Pass, Host, Port, VSID):
-        accepted = self.accepted
-        tn = self.tn
+        self.tn.open(Host, Port)
+        self.tn.read_until(".", 2) #wait until the welcome message is displayed
         
-        tn.open(Host, Port)
-        tn.read_until(".", 2) #wait until the welcome message is displayed
-        
-        tn.write("login " + User + " " + Pass + "\n")   #send login string
-        msg = tn.read_until(accepted, 2)                #wait for login to be accepted or denied
+        self.tn.write("login " + User + " " + Pass + "\n")   #send login string
+        msg = self.tn.read_until(self.accepted, 2)           #wait for login to be accepted or denied
         
         #if login is accepted select the virtual server
-        if msg == accepted:
-            tn.write("use " + VSID + "\n")
-            msg = tn.read_until(accepted, 2)
+        if msg == self.accepted:
+            self.tn.write("use " + VSID + "\n")
+            msg = self.tn.read_until(self.accepted, 2)
             
             #if command is not accepted disconnect and return false
-            if msg != accepted:
+            if msg != self.accepted:
                 print "\n\rError selecting virtual server " + VSID
-                tn.write("logout\n")
-                tn.read_until(accepted, 2)
-                tn.write("quit\n")
+                self.tn.write("logout\n")
+                self.tn.read_until(self.accepted, 2)
+                self.tn.write("quit\n")
                 return False
             return True
         else:
             sys.stdout.write("\r\nError logging in. Please check your username and password.")
             sys.stdout.write(msg.replace("\s", " "))
-            tn.write("quit\n")
+            self.tn.write("quit\n")
             return False
         
-        
+    #gets a list of connected clients
     def clientlist(self):
-        #send the clientlist command and get the output
         self.tn.write("clientlist\n")
         msg = self.tn.read_until(self.accepted, 2)
-        
-        msg = msg.replace(self.accepted, "") #remove the command accepted string
-        msg = msg.replace("\n\r", "")   #remove line breaks
-        msg = msg.replace("|", " ")     #replace vertical bars with spaces
+        msg = msg.replace(self.accepted, "")
+        msg = msg.replace("\n\r", "")
+        msg = msg.replace("|", " ")
         msg = msg.split(' ')
         
         clist = []
@@ -58,7 +55,7 @@ class TS3():
             if c.find('clid=') > -1:
                 clist.append(c.replace('clid=',''))
         return clist
-        
+
 
     def clientinfo(self, client):
         #send clientinfo command and get the output
@@ -70,21 +67,27 @@ class TS3():
         msg = msg.split(' ')
         
         c = TS3Client()
-        
         c.clid = client
         #find the client idle time
         for m in msg:
             if m.find('cid=') > -1:
                 c.cid = m.replace('cid=','')
+                continue
                 
             if m.find('client_idle_time=') > -1:
                 c.idletime = int(m.replace('client_idle_time=','')) / 60000
+                continue
                 
             if m.find('client_servergroups=') > -1:
                 c.servergroups = m.replace('client_servergroups=','').split(',')
+                continue
                 
             if m.find('client_away=') > -1:
                 c.away = m.replace('client_away=','')
+                continue
+                
+            if m.find('client_type=') > -1:
+                c.type = m.replace('client_away=','')
         return c
         
         
